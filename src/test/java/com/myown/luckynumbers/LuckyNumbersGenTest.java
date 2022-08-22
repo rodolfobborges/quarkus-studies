b93cf449a7d36835335e241e5d2ffb256ff04d02
Rodolfo Borges <rodolfobborges@gmail.com>
2022-08-22 20:34:52 +0100

    Adding more tests

--- src/test/java/com/myown/luckynumbers/LuckyNumbersGenTest.java
+++ src/test/java/com/myown/luckynumbers/LuckyNumbersGenTest.java
@@ -3,6 +3,7 @@ package com.myown.luckynumbers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -24,11 +25,36 @@ class LuckyNumbersGenTest {
     }
 
     @Test
-    void shouldReturnACollectionWithMoreThanOneSequenceOfSixSortedRandomIntegersTillSixty() {
+    void shouldReturnACollectionWithTwoSequencesOfSixSortedRandomIntegersTillSixty() {
 
         HashMap<Integer, List<Integer>> sequences = gen.moreThanOneSixSortedRandomIntegersUpToSixty(2);
         assertEquals(2, sequences.size());
         assertEquals(6, sequences.get(0).size());
         assertEquals(6, sequences.get(1).size());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenRequestedSequencesPassMaximumAllowed(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> gen.moreThanOneSixSortedRandomIntegersUpToSixty(100));
+
+        assertEquals("Max of how many 6 random integers sequences is " + LuckyNumbersGen.HOW_MANY_MAX,
+                exception.getMessage());
+    }
+
+    @Test
+    void shouldReturnSixSortedRandomIntegersIncludingGivenNumbers() {
+
+        List<Integer> givenNumbers = Arrays.asList(2,29,15,5,41);
+        List<Integer> sequence = gen.fillUpUntilSixSortedRandomIntegersUpToSixty(givenNumbers);
+        assertEquals(6, sequence.size());
+        assertTrue(sequence.contains(2));
+        assertTrue(sequence.contains(5));
+        assertTrue(sequence.contains(15));
+        assertTrue(sequence.contains(29));
+        assertTrue(sequence.contains(41));
+
+        // this line is just for debugging purpose
+        // gen.fillUpUntilSixSortedRandomIntegersUpToSixty(givenNumbers).forEach(System.out::println);
     }
 }
