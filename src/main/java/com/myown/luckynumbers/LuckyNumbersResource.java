94890b21b175656a596f984f14caaf50870f4712
Rodolfo Borges <rodolfobborges@gmail.com>
2024-01-11 18:15:13 +0000

    Add proper http response code

--- src/main/java/com/myown/luckynumbers/LuckyNumbersResource.java
+++ src/main/java/com/myown/luckynumbers/LuckyNumbersResource.java
@@ -6,6 +6,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+import javax.ws.rs.core.Response;
 
 @Path("/gen")
 public final class LuckyNumbersResource {
@@ -16,21 +18,34 @@ public final class LuckyNumbersResource {
     @GET
     @Path("/6upto60")
     @Produces(MediaType.TEXT_PLAIN)
-    public String sixUpToSixty() {
-        return luckyNumbersGen.sixSortedRandomIntegersUpToSixty().toString();
+    public Response sixUpToSixty() {
+        return Response.ok(
+                luckyNumbersGen.sixSortedRandomIntegersUpToSixty().toString()
+        ).build();
     }
 
     @GET
     @Path("/6upto60/{howManySequences}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String sixUpToSixty(@PathParam("howManySequences") final Integer howManySequences) {
-        return luckyNumbersGen.moreThanOneSixSortedRandomIntegersUpToSixty(howManySequences).toString();
+    public Response sixUpToSixty(@PathParam("howManySequences") final Integer howManySequences) {
+        try {
+            return Response.ok(
+                    luckyNumbersGen.moreThanOneSixSortedRandomIntegersUpToSixty(howManySequences).toString()
+            ).build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
     }
 
     @GET
     @Path("/6upto60/with/{numbers}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String sixUpToSixtyWithGiven(@PathParam("numbers") final String numbers) {
-        return luckyNumbersGen.fillUpUntilSixSortedRandomIntegersUpToSixty(numbers).toString();
+    public Response sixUpToSixtyWithGiven(@PathParam("numbers") final String numbers) {
+        try {
+            List<Integer> result = luckyNumbersGen.fillUpUntilSixSortedRandomIntegersUpToSixty(numbers);
+            return Response.ok(result.toString()).build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
     }
 }
--- src/test/java/com/myown/luckynumbers/LuckyNumbersResourceTest.java
+++ src/test/java/com/myown/luckynumbers/LuckyNumbersResourceTest.java
@@ -4,6 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
 @QuarkusTest
@@ -31,6 +32,14 @@ class LuckyNumbersResourceTest {
                 .when().get("/gen/6upto60/with/2,6")
                 .then().statusCode(200)
                 .body(notNullValue());
+    }
+
+    @Test
+    void sixUpToSixtyWithNegativeGivenNumberMustFail() {
+        given()
+                .when().get("/gen/6upto60/with/-7")
+                .then().statusCode(400)
+                .body(containsString("Numbers must be greater than zero"));
     }
 
 }
